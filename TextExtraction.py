691421aebd0936a5e74aa633d00d8cca7e2133c8
Ankita Agharkar <Ankitaa@WinjitTechnologiesPvtLtd.local>
2017-11-01 14:45:59 +0530

    crop_img.py

--- TextExtraction.py
+++ TextExtraction.py
@@ -8,7 +8,7 @@ from multiprocessing import Queue
 import Img_to_Text,crop_img,Licence_Details
 
 UPLOAD_FOLDER = 'Upload_Image\\'
-ALLOWED_EXTENSIONS = set(['jpg','jpeg','JPG','PNG','PDF','JPEG'])
+ALLOWED_EXTENSIONS = set(['jpg','jpeg','JPG','PNG','PDF','JPEG','bmp','BMP'])
 app = Flask(__name__,static_folder='static')
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 sess=Session()
@@ -28,24 +28,29 @@ def index():
 
 @app.route("/show", methods=['POST'])
 def show():
-    if request.method == 'POST':
-        file = request.files['file']
-        if file and allowed_file(file.filename):
-            filename = werkzeug.secure_filename(file.filename)
-            filename = str(filename).lower()
-            file.save(app.config['UPLOAD_FOLDER']+filename)
-            image = app.config['UPLOAD_FOLDER']+filename
-            image_path=crop_img.get_cropped_image(image)
-            print(image_path)
-            head, tail = os.path.split(image_path)
-            thread = threading.Thread(target=get_doc, args=(image_path,))
-            thread.start()
-            text= output_doc.get()
-            licence_id,dob=Licence_Details.get_licence_details(text)
-        else:
-            error = "Please Upload jpg or png image"
-            return render_template('Index.html', error=error)
-        return render_template('Show.html', text=text,image=tail,licence_id=licence_id,dob=dob)
+    try:
+        if request.method == 'POST':
+            file = request.files['file']
+            if file and allowed_file(file.filename):
+                filename = werkzeug.secure_filename(file.filename)
+                filename = str(filename).lower()
+                file.save(app.config['UPLOAD_FOLDER']+filename)
+                image = app.config['UPLOAD_FOLDER']+filename
+                image_path=crop_img.get_cropped_image(image)
+                print(image_path)
+                head, tail = os.path.split(image_path)
+                thread = threading.Thread(target=get_doc, args=(image_path,))
+                thread.start()
+                text= output_doc.get()
+                licence_id,max_date,min_date,iss_date=Licence_Details.get_licence_details(text)
+            else:
+                error = "Please Upload jpg or png image"
+                return render_template('Index.html', error=error)
+            # return render_template('Show.html', text=text,image=tail)
+            return render_template('Show.html', text=text,image=tail,licence_id=licence_id,max_date=max_date,min_date=min_date,iss_date=iss_date)
+    except Exception as E:
+        error = "Licence ID not Detected"
+        return render_template('Index.html', error=error)
 
 if __name__ == "__main__":
     app.secret_key = 'super secret key'
